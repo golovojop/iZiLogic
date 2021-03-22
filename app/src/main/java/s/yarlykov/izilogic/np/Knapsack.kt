4d76210507868ec75a06f25521ef6d1cba45f8b0
ciscoff <ciscoff@ya.ru>
2021-03-22 18:59:51 +0300

    knapsack ok

--- app/src/main/java/s/yarlykov/izilogic/np/Knapsack.kt
+++ app/src/main/java/s/yarlykov/izilogic/np/Knapsack.kt
@@ -17,6 +17,11 @@ class Knapsack(
         return selection.asList()
     }
 
+    /**
+     * @param i - индекс текущего элемента
+     * @param acc - накопитель
+     * @param selection - должен содержать список выбранных элементов. Не работает в этой версии.
+     */
     private fun pack(i: Int, acc: Int, selection: Array<Int>): Int {
         if (acc > capacity) return Int.MIN_VALUE
         if (i > items.lastIndex || acc == capacity) return acc
@@ -28,16 +33,56 @@ class Knapsack(
         val activeRes = pack(i + 1, acc + items[i], selection)
 
         if (activeRes == capacity) {
-            println ("i=$i, value=${items[i]}")
+            println("i=$i, value=${items[i]}")
             selection[i] = i
         }
         return max(passiveRes, activeRes)
     }
-}
 
+    fun packWithHistory(): ArrayList<ArrayList<Int>>? {
+        return packWithHistoryLoop(i = 0, acc = 0)
+    }
+
+    private fun packWithHistoryLoop(
+        i: Int,
+        acc: Int
+    ): ArrayList<ArrayList<Int>>? {
+
+        when {
+            // Результат достигнут. Создать новый список для записи тех, кто участвовал в
+            // суммировании общего результата. Список будет заполняться по мере всплытия.
+            (acc == capacity) -> {
+                return ArrayList<ArrayList<Int>>().apply { add(ArrayList()) }
+            }
+
+            // Перелёт: дальше нет смысла продолжать. Или прошли всё, но результат плохой.
+            (acc > capacity) || (i > items.lastIndex) -> return null
+        }
+
+        // Обход ветки где я НЕ участвую
+        val leftTraversal = packWithHistoryLoop(i + 1, acc + 0)
+        // Обход ветки где я УЧАСТВУЮ (acc + items[i])
+        val rightTraversal = packWithHistoryLoop(i + 1, acc + items[i])
+
+        // Поместить себя во все подмассивы результата правой ветки
+        rightTraversal?.forEach {
+            it.add(i)
+        }
+
+        // Теперь нужно смержить подмассивы обеих веток в один список подмассивов
+        return rightTraversal?.also { right -> leftTraversal?.let(right::addAll) } ?: leftTraversal
+    }
+}
 
 fun main() {
     val knapsack = Knapsack(11, listOf(1, 2, 3, 4, 7, 5))
 //    val knapsack = Knapsack(4, listOf(1, 2, 3))
-    val list = knapsack.pack()
+
+//    val list = knapsack.pack()
+
+    val r = knapsack.packWithHistory()
+
+    r?.forEach {
+        println(it.toString())
+    }
 }
