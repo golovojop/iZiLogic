8a56f6b5d35bf2a00336f49c718209598b93dbd2
ciscoff <ciscoff@ya.ru>
2021-04-03 22:43:10 +0300

    +

--- app/src/main/java/s/yarlykov/izilogic/leetcode/add_two_numbers.kt
+++ app/src/main/java/s/yarlykov/izilogic/leetcode/add_two_numbers.kt
@@ -1,4 +1,50 @@
 package s.yarlykov.izilogic.leetcode
 
-class add_two_numbers {
+import kotlin.math.pow
+
+/**
+ * https://leetcode.com/problems/add-two-numbers/
+ *
+ * You are given two non-empty linked lists representing
+ * two non-negative integers. The digits are stored in reverse order,
+ * and each of their nodes contains a single digit. Add the two numbers
+ * and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except
+ * the number 0 itself.
+ */
+
+val a342 = listOf(2, 4, 3) // Это запись числа '342'
+val b465 = listOf(5, 6, 4) // Это запись числа '465'
+
+/**
+ * Превратить список Int'ов, которые представляют собой цифры трехзначного положительного
+ * числа, записанные в обратном порядке, в это самое трехзначное число.
+ */
+fun List<Int>.listToNum(): Int {
+    return mapIndexed { index, n -> n * 10.toFloat().pow(index).toInt() }.sum()
+}
+
+fun main() {
+
+    var overhead = 0
+
+    /**
+     * Обычное сложение столбиком.
+     * overhead - для учета переноса единицы в следующий разряд.
+     */
+    val number = a342.zip(b465) { a, b ->
+        var sum = a + b + overhead
+
+        if (sum >= 10) {
+            overhead = 1
+            sum -= 10
+        } else {
+            overhead = 0
+        }
+
+        sum
+
+    }.listToNum()
+
+    println("$number")
 }
--- app/src/main/java/s/yarlykov/izilogic/leetcode/two_sum.kt
+++ app/src/main/java/s/yarlykov/izilogic/leetcode/two_sum.kt
@@ -1,4 +1,32 @@
 package s.yarlykov.izilogic.leetcode
 
-class two_sum {
+import kotlin.math.max
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ *
+ *  Given an array of integers nums and an integer target, return indices
+ *  of the two numbers such that they add up to target. You may assume that
+ *  each input would have exactly one solution, and you may not use the same
+ *  element twice.
+ *  You can return the answer in any order.
+ */
+
+val numbers = /*listOf(2, 7, 11, 15)*/ listOf(3, 2, 4)
+const val target = /*9 */6
+
+fun search(acc: Int, i: Int): Int {
+    if (acc == target) return target
+    if (acc > target || i >= numbers.size) return Int.MIN_VALUE
+
+    val leftSearch = search(acc, i + 1)
+    val rightSearch = search(acc + numbers[i], i + 1)
+
+    if (rightSearch == target) println("$i")
+
+    return max(leftSearch, rightSearch)
+}
+
+fun main() {
+    search(0, 0)
 }
