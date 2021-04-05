7256d5fcea1625771423c95c29f2af9dab346efa
ciscoff <ciscoff@ya.ru>
2021-04-05 11:25:36 +0300

    +

--- app/src/main/java/s/yarlykov/izilogic/leetcode/t003_longest_substring.kt
+++ app/src/main/java/s/yarlykov/izilogic/leetcode/t003_longest_substring.kt
@@ -1,4 +1,36 @@
 package s.yarlykov.izilogic.leetcode
 
-class t003_longest_substring {
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * Given a string s, find the length of the longest substring without repeating characters.
+ */
+
+fun longestSubstring(sz : String = "abcabcdd") : String{
+
+    val charSet = hashSetOf<Char>()
+    var lastIndex = 0
+    var longestSz = ""
+
+    sz.forEachIndexed { i, ch ->
+
+        if(charSet.contains(ch)) {
+            if(i - lastIndex > longestSz.length) {
+                longestSz = sz.substring(lastIndex, i)
+            }
+            lastIndex = i
+            charSet.clear()
+            charSet.add(ch)
+        } else {
+            charSet.add(ch)
+        }
+    }
+
+    return longestSz
+}
+
+fun main() {
+
+    val longestSz = longestSubstring()
+    println("Length of the longest substring is ${longestSz.length}")
 }
