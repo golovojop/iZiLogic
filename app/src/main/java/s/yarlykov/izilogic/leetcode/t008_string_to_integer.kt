fb68429833c84241dc40537ac53437630520cfc5
ciscoff <ciscoff@ya.ru>
2021-04-09 17:42:52 +0300

    +

--- app/src/main/java/s/yarlykov/izilogic/leetcode/t008_string_to_integer.kt
+++ app/src/main/java/s/yarlykov/izilogic/leetcode/t008_string_to_integer.kt
@@ -1,26 +1,40 @@
 package s.yarlykov.izilogic.leetcode
 
+/**
+ * https://leetcode.com/problems/string-to-integer-atoi/
+ *
+ * Сконвертить строку в число соблюдая заданные условия (см. url)
+ */
 fun strToInt(str: String): Int {
 
-    val clearStr = str.trim()
+    val trimmedStr = str.trim()
     var startFrom = 0
     var sign = 1
 
-    if (clearStr.isNotEmpty()) {
-        if (clearStr[0] == '-') {
+    if (trimmedStr.isNotEmpty()) {
+        if (trimmedStr[0] == '-') {
             sign = -1
             startFrom = 1
         }
-        if (clearStr[0] == '+') startFrom = 1
+        if (trimmedStr[0] == '+') startFrom = 1
     }
 
     var result = 0
 
-    for(i in startFrom until clearStr.length) {
-        if (clearStr.first() !in '0'..'9') break
+    for (i in startFrom until trimmedStr.length) {
+        if (trimmedStr[i] !in '0'..'9') break
 
         result *= 10
-        result += clearStr[i].toInt()
+        val nextDigit = Character.getNumericValue(trimmedStr[i])
+
+        if (sign > 0 && nextDigit > (Int.MAX_VALUE - result)) {
+            return Int.MAX_VALUE
+        }
+        if (sign < 0 && nextDigit > (Int.MAX_VALUE - result)) {
+            return Int.MIN_VALUE
+        }
+
+        result += nextDigit
     }
 
     return result * sign
@@ -28,5 +42,9 @@ fun strToInt(str: String): Int {
 
 
 fun main() {
-    println("${strToInt("123")}")
+    // MAX_VALUE: Int = 2147483647 (это 2^31 - 1)
+    // MIN_VALUE: Int = -2147483648 (это -2^31)
+
+    println("${strToInt("+00123a456")}")
+    println("${strToInt("-2147483647")}")
 }
--- app/src/main/java/s/yarlykov/izilogic/leetcode/t009_palindrome_number.kt
+++ app/src/main/java/s/yarlykov/izilogic/leetcode/t009_palindrome_number.kt
@@ -1,4 +1,32 @@
 package s.yarlykov.izilogic.leetcode
 
-class t009_palindrome_number {
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ *
+ *
+ */
+
+fun isPalindromeNumber(num: Int): Boolean {
+
+    if(num < 0) return false
+
+    val array = ArrayList<Int>()
+
+    var next = num
+    var i = 0
+
+    while (next != 0) {
+        array.add(i++, next % 10)
+        next /= 10
+    }
+
+    for (j in 0 until array.size / 2) {
+        if (array[j] != array[array.size - j - 1]) return false
+    }
+
+    return true
+}
+
+fun main() {
+    println(isPalindromeNumber(+1212121))
 }
